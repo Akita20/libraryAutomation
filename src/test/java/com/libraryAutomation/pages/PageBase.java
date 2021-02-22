3bfb1546ba3d4a876bfbf9feee4121fe1fd2a31d
esma17 <milkysf@gmail.com>
2021-02-22 16:09:36 -0600

    added PageGenerator.java

--- src/test/java/com/libraryAutomation/pages/PageBase.java
+++ src/test/java/com/libraryAutomation/pages/PageBase.java
@@ -5,10 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public abstract class PageBase {
-    public PageBase(){
-        PageFactory.initElements(Driver.getDriver(),this);
-    }
+public abstract class PageBase extends PageGenerator {
+
     @FindBy(linkText = "Users")
     public WebElement usersPageLink;
 
